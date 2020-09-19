2c349e72002e73f3c6db8979561abb1c8b4cbead
sai-myne <ahlusir@gmail.com>
2020-09-19 11:12:31 +0800

    Front End Login & Register Pages

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,19 +3,19 @@ const usersResolvers = require('./users');
 const commentsResolvers = require('./comments');
 
 module.exports = {
-    Post: {
-        likeCount:(parent) => parent.likes.length,
-        commentCount: (parent) => parent.comments.length
-    },
-    Query: {
-        ...postsResolvers.Query
-    },
-    Mutation: {
-        ...usersResolvers.Mutation,
-        ...postsResolvers.Mutation,
-        ...commentsResolvers.Mutation
-    },
-    Subscription: {
-        ...postsResolvers.Subscription
-    }
+  Post: {
+    likeCount: (parent) => parent.likes.length,
+    commentCount: (parent) => parent.comments.length
+  },
+  Query: {
+    ...postsResolvers.Query
+  },
+  Mutation: {
+    ...usersResolvers.Mutation,
+    ...postsResolvers.Mutation,
+    ...commentsResolvers.Mutation
+  },
+  Subscription: {
+    ...postsResolvers.Subscription
+  }
 };
